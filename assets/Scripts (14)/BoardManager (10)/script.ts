b69dcd2260717f5f8bc3492cd2c628171a3d5be2
Namerian <patrickmonville@hotmail.com>
2016-12-27 15:21:05 +0100

    stuff

--- assets/Scripts (14)/BoardManager (10)/script.ts
+++ assets/Scripts (14)/BoardManager (10)/script.ts
@@ -1,19 +1,39 @@
+enum Player{
+      Red,
+      Black
+}
+
 class BoardManagerBehavior extends Sup.Behavior {
-  pawn: Sup.Actor[];
+  pawn: PawnControllerBehavior[];
   
   awake() {
-    
+    this.pawn = new Array<PawnControllerBehavior>();
   }
   
   start(){
-    this.pawn= Sup.appendScene("Prefabs/PawnPrefab", this.actor);
-    
-    this.pawn[0].getBehavior(PawnControllerBehavior).initialize();
-    this.pawn[0].setLocalPosition(0.5,0.5,1);
+    let actors = Sup.appendScene("Prefabs/PawnPrefab", this.actor);
+    if(actors.length === 1){
+      let newPawnController = actors[0].getBehavior(PawnControllerBehavior);
+      newPawnController.initialize(Player.Red, {x:0,y:0});
+      this.pawn.push(newPawnController);
+    }
   }
 
   update() {
-    
+    if(Sup.Input.wasMouseButtonJustPressed(0)){
+      let mousePos = Sup.Input.getMousePosition();
+      if(mousePos.x >= -5 && mousePos.x <= 5 && mousePos.y >= -5 && mousePos.y <= 5){
+        let mousedOverTile = {x:Math.floor(mousePos.x + 5), y:Math.floor(mousePos.y + 5)};
+      }
+    }
+  }
+  
+  private GetPawnByPos(pos:Sup.Math.XY) :PawnControllerBehavior{
+    for(let pawn of this.pawn){
+      if(pawn.position.x === pos.x && pawn.position.y === pos.y){
+        return pawn;
+      }
+    }
   }
 }
 Sup.registerBehavior(BoardManagerBehavior);
--- assets/Scripts (14)/PawnController (11)/script.ts
+++ assets/Scripts (14)/PawnController (11)/script.ts
@@ -1,4 +1,7 @@
 class PawnControllerBehavior extends Sup.Behavior {
+  player :Player;
+  position :Sup.Math.XY;
+  
   awake() {
     
   }
@@ -7,8 +10,23 @@ class PawnControllerBehavior extends Sup.Behavior {
     
   }
   
-  public initialize(){
-    let spriteName = "Sprites/BlackPawnSprite";
+  public initialize(player:Player, boardPos:Sup.Math.XY){
+    this.player = player;
+    this.position = {x:boardPos.x, y:boardPos.y};
+    
+    //set Position
+    let screenPos = {x:this.position.x + 0.5, y:this.position.y + 0.5, z:1};
+    this.actor.setLocalPosition(screenPos);
+    
+    //set Sprite
+    let spriteName;
+    
+    if(player === Player.Red){
+      spriteName = "Sprites/RedPawnSprite";
+    }
+    else if(player === Player.Black){
+      spriteName = "Sprites/BlackPawnSprite";
+    }
     
     this.actor.spriteRenderer.setSprite(spriteName);
   }
